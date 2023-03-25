c65412781d2a1936f519bf7293a7b2ebef576fa6
pangestu <agung.pangestu@fr8labs.co>
2023-03-25 20:34:31 +0700

    feat: add pre commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
